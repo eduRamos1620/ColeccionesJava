87a9f299fb6f787fa78c3d5be52fa0c0102a4464
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-12-14 21:26:57 -0600

    Validando con Set si hay elementos duplicados

--- src/com/eduardo/ejemplos/set/EjemploHashSetBuscarDuplicado.java
+++ src/com/eduardo/ejemplos/set/EjemploHashSetBuscarDuplicado.java
@@ -1,7 +1,18 @@
 package com.eduardo.ejemplos.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EjemploHashSetBuscarDuplicado {
     public static void main(String[] args) {
-        String [] peces = {"Covina", "Lenguado", "Pejerrey", "Robalo"};
+        String [] peces = {"Covina", "Lenguado", "Pejerrey", "Robalo", "Atun", "Lenguado"};
+
+        Set<String> unicos = new HashSet<>();
+        for (String pez: peces){
+            if (!unicos.add(pez)){
+                System.out.println("Elemento duplicado " + pez);
+            }
+        }
+        System.out.println(unicos.size() + " elementos no duplicado " + unicos);
     }
 }
