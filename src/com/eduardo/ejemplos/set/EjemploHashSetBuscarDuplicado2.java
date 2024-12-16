e8cbd3c15e58b223d88007eff86e47a45d03e290
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-12-15 18:12:01 -0600

    Se agrega nuevo set para separar los elemntos duplicados

--- src/com/eduardo/ejemplos/set/EjemploHashSetBuscarDuplicado2.java
+++ src/com/eduardo/ejemplos/set/EjemploHashSetBuscarDuplicado2.java
@@ -3,16 +3,19 @@ package com.eduardo.ejemplos.set;
 import java.util.HashSet;
 import java.util.Set;
 
-public class EjemploHashSetBuscarDuplicado {
+public class EjemploHashSetBuscarDuplicado2 {
     public static void main(String[] args) {
         String [] peces = {"Covina", "Lenguado", "Pejerrey", "Robalo", "Atun", "Lenguado", "Pejerrey"};
 
         Set<String> unicos = new HashSet<>();
+        Set<String> duplicados = new HashSet<>();
         for (String pez: peces){
             if (!unicos.add(pez)){
-                System.out.println("Elemento duplicado " + pez);
+                duplicados.add(pez);
             }
         }
-        System.out.println(unicos.size() + " elementos no duplicado " + unicos);
+        unicos.removeAll(duplicados);
+        System.out.println("Unicos " + unicos);
+        System.out.println("Duplicados " + duplicados);
     }
 }
