f61084c3748f2aee410466b1b11e59968eb5948b
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-12-22 18:12:17 -0600

    Se agregan ejemplos de metodos en LinkedList

---
+++ src/com/eduardo/ejemplos/set/EjemploLinkedList.java
@@ -0,0 +1,33 @@
+package com.eduardo.ejemplos.set;
+
+import com.eduardo.ejemplos.modelo.Alumno;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjemploLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Alumno> enlazada = new LinkedList<>();
+        System.out.println(enlazada + " size: " + enlazada.size());
+        System.out.println("esta vacia: " + enlazada.isEmpty());
+        enlazada.add(new Alumno("Juan", 5));
+        enlazada.add(new Alumno("Pedro", 9));
+        enlazada.add(new Alumno("Julia", 6));
+        enlazada.add(new Alumno("Luna", 8));
+        enlazada.add(new Alumno("Toño", 4));
+        System.out.println(enlazada + " size: " + enlazada.size());
+
+        enlazada.addFirst(new Alumno("Chema", 6));
+        enlazada.addLast(new Alumno("Pepe", 7));
+        System.out.println(enlazada + " size: " + enlazada.size());
+
+        System.out.println("Primer elemento: " + enlazada.getFirst());
+        System.out.println("Ultimo elemento: " + enlazada.getLast());
+
+        enlazada.removeFirst();
+        enlazada.removeLast();
+
+        System.out.println(enlazada + " size: " + enlazada.size());
+    }
+}
